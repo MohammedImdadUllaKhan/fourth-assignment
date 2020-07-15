2618838478c455daf6e513476b3d2f19c742bf66
Mohammed Imdad Ulla Khan <68274252+MohammedImdadUllaKhan@users.noreply.github.com>
2020-07-15 14:16:51 +0530

    Add files via upload

---
+++ varlrtconst.js
@@ -0,0 +1,26 @@
+// variables
+
+var name ="imdad";
+var age = 23;
+var canswim = false;
+var language =["english","hindi","kannada"]
+var friends ={name:"sulaiman",hobby:"pubg"}
+console.log(name)
+console.log(age)
+console.log(canswim)
+console.log(language)
+console.table(friends)
+language.push("urdu")
+
+// constant
+
+const country= "INDIA";
+console.log(country)
+
+const tallest_mountain="Mount EVEREST"
+console.log(tallest_mountain)
+
+// let
+
+let farhan = name
+console.log(farhan)
